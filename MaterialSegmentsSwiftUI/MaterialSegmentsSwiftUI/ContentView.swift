408238143f2004ac9d2d2aad2b58ff50a567ddb7
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-11-14 19:14:47 +0530

    Create: MaterialSegmentsSwiftUI

---
+++ MaterialSegmentsSwiftUI/MaterialSegmentsSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaterialSegmentsSwiftUI
+//
+//  Created by aakarshit on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaterialSegmentsSwiftUI/MaterialSegmentsSwiftUI/MaterialSegmentsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialSegmentsSwiftUIApp.swift
+//  MaterialSegmentsSwiftUI
+//
+//  Created by aakarshit on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MaterialSegmentsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
