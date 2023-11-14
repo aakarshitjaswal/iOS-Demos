aa7c05f5c63a1b668428c27ea901dd4afe01e5b2
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-11-14 19:22:44 +0530

    Create: Self centering Segment view

--- MaterialSegmentsSwiftUI/MaterialSegmentsSwiftUI/ContentView.swift
+++ MaterialSegmentsSwiftUI/MaterialSegmentsSwiftUI/ContentView.swift
@@ -8,17 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var segments = ["Profile",
+                           "Allergies",
+                           "Appointments",
+                           "Documents",
+                           "Encounters",
+                           "Medications",
+                           "Notes",
+                           "Labs",
+                           "Payments",
+                           "Prescriptions",
+                           "Questionnaires",
+                           "Supplements"]
+    
+    @State var selectedSegment = "Profile"
+    @Namespace var animation
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            VStack {
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(systemName: "arrow.backward")
+                            .font(.system(size: 17))
+                            .foregroundColor(.green)
+                    }
+                    .padding(.leading)
+                    
+                    Spacer()
+                    Text("Patient Details")
+                        .foregroundColor(.green)
+                        .font(.system(size: 17))
+                    Spacer()
+                    
+                    Image(systemName: "arrow.backward")
+                        .font(.system(size: 17))
+                        .foregroundColor(.white)
+                        .padding(.trailing)
+                    
+                }
+                .padding(.top)
+                
+                ScrollViewReader { reader in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            HStack(spacing: 0) {
+                                ForEach(segments, id: \.self) { segment in
+                                    Button {
+                                        withAnimation {
+                                            selectedSegment = segment
+//                                            reader.scrollTo(segment)
+                                            reader.scrollTo(segment, anchor: .center)
+                                        }
+                                    } label: {
+                                            VStack(spacing: 2) {
+                                                Text(segment)
+                                                    .font(.system(size: 15))
+                                                if selectedSegment == segment {
+                                                    RoundedRectangle(cornerRadius: 1)
+                                                        .frame(height: 2)
+                                                        .matchedGeometryEffect(id: "segment", in: animation)
+                                                }
+                                            }
+                                            .foregroundColor(selectedSegment == segment ? .blue : .black)
+                                        
+                                    }
+                                    .accentColor(.primary)
+                                 }
+                                .padding(.horizontal, 7)
+                            }
+                        }
+                        .scrollIndicators(.hidden)
+                        
+                    }
+                    Spacer()
+                }
+            }
         }
-        .padding()
+    }
+    
+    struct PatientSegment: Hashable {
+        let name: String
+        var isSelected: Bool = false
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-#Preview {
-    ContentView()
-}
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # iOS-Demos
+
+1. Timer - A Timer application with timer updating inside of push notifications
+2. MaterialSegmentsSwiftUI - Demo for creating Segment view using ScrollViewReader and Matched geometry for centering the selected segment.
